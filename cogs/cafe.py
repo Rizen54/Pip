121731a7446eeb7b09087ff60bedc6d82be60274
Rizen54 <psatvik54@proton.me>
2024-11-02 12:10:26 +0530

    Initial virtual cafe

--- cogs/cafe.py
+++ cogs/cafe.py
@@ -12,23 +12,31 @@ class Cafe(commands.Cog):
 
     @app_commands.command(name="menu", description="Displays cafe menu.")
     async def menu(self, interaction: discord.Interaction):
-        """Displays a list of available commands in an embed."""
+        """Displays a list of available Food items in the cafe."""
     
         menu_list = [
-        "*Coffee* - `$5`",
-        "*Sandwiches* - `$7`",
-        "*Burger* - `$3`",
+            ":coffee: *Coffee* - `Free`",
+            ":sandwich: *Sandwich*- `$4`",
+            ":pizza: *Pizza* - `$5`",
+            ":hamburger: *Burger* - `$3`",
         ]
 
         embed = discord.Embed(
-            title="📜 𝓶𝓮𝓷𝓾",
-            description="What would you like to have?!",
-            color=discord.Color.brand_red(),
+            title="📜 ***Cafe Menu***",
+            description="What would you like to have?",
+            color=discord.Color.dark_gold(),
         )
 
         embed.add_field(name="Options", value="\n".join(menu_list), inline=False)
         await interaction.response.send_message(embed=embed)
 
+
+    @app_commands.command(name="order", description="Places order for a food item.")
+    async def order(self, interaction: discord.Interaction, option: str):
+        menu = {"coffee": "free", "sandwich": "$4", "burger": "$3", "pizza": "$5"}
+        if option.lower() in menu:
+            await interaction.response.send_message(f"Here's your *{option.capitalize()}* for `{menu[option]}`!")
+        
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Cafe(bot))
